80a1a5ad70af4e408d3e2af51ba8b6645a273073
Ralf Popescu <ralfpopescu@gmail.com>
2017-01-15 19:41:03 -0500

    downloading works

--- server.js
+++ server.js
@@ -2,20 +2,23 @@ var express = require('express');
 var app = express();
 
 var bodyParser = require('body-parser');
+
 app.use(bodyParser.json()); // support json encoded bodies
 app.use(bodyParser.urlencoded({ extended: true })); // support encoded bodies
+app.use(bodyParser());
 
 app.use(express.static('public'));
 app.listen(8080, function() { console.log('listening')});
 
 app.post('/featherclick', function (req, res) {
-  var activated = req.body.activated
-  var result = matchKey(activated);
-  if(result == "key1"){
+  // var activated = req.body;
+  // var result = matchKey(activated);
+  // if(result == "key1"){
     //res.send('secrets/ganzremix.mp3');
-    res.send("http://www.soundcloud.com");
+    res.setHeader("Content-Type","application/octect-stream");
+    res.download("secrets/ganzremix.mp3");
     console.log("MATCH");
-}
+//}
 });
 
 
